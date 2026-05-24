0eff0643277966e71618f2278ba8257e879d15f8
Sito8943 <sito8943@gmail.com>
2026-05-24 15:58:20 +0200

    icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,3 +1,5 @@
+
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5"
 import { Tabs } from "expo-router"
 import { type ReactElement } from "react"
 
@@ -6,10 +8,32 @@ export default function Layout(): ReactElement {
     <Tabs>
       <Tabs.Screen
         name="transactions"
-        options={{ title: "Transactions", headerShown: false }}
+        options={{
+          title: "Transactions",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 size={20} name="file-invoice-dollar" color={color} />
+          ),
+        }}
       />
-      <Tabs.Screen name="categories" options={{ title: "Categories" }} />
-      <Tabs.Screen name="currencies" options={{ title: "Currencies" }} />
+      <Tabs.Screen
+        name="categories"
+        options={{
+          title: "Categories",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 size={20} name="tags" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="currencies"
+        options={{
+          title: "Currencies",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="coins" color={color} size={20} />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
